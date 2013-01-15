ffc08fae6b499fa8b2b1f66f3d224fe8ce3354e0
Henrik Larsen Toft <henrik.larsenoid@gmail.com>
2013-01-15 20:21:48 +0100

    Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Casetv::Application.config.secret_token = 'b96f6f03d06e4b50f5a1f162dbef5bee7910e81941860e6179cbd4946fb78e3276c89339f2d09ddddf5dfa233b33a61d74d90ae0cafc0a98f88d4fc5fddfebe2'
